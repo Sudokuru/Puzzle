a83be064be16f2fbc45ee3847371ea991d0375ba
Galla <gallant.thomas@outlook.com>
2023-02-23 11:35:21 -0500

    Adding dotenv to allow process.env to work correctly.

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -9,6 +9,7 @@
 const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
 const express = require("express");
 const routes = express.Router();
+require('dotenv').config()
 
 // Authorization middleware. When used, the Access Token must
 // exist and be verified against the Auth0 JSON Web Key Set.
