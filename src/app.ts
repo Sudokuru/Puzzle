925498314f41bbbc5a8b9206cb7616c086b36973
Galla <gallant.thomas@outlook.com>
2023-03-26 21:10:31 -0400

    Adding warming function

--- src/app.ts
+++ src/app.ts
@@ -28,6 +28,10 @@ app.use(expressApp.json());
 
 const baseRoute: string = "/api/v1";
 
+app.get(baseRoute, function (req, res) {
+    res.send("Puzzle");
+});
+
 const globalRouter = require('./app/routes/routes');
 app.use(baseRoute, globalRouter);
 
